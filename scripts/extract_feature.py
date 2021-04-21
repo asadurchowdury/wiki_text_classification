e7d26a8a668ed3d7a9f79db4277248f79eea40f3
asadurchowdury <69541380+asadurchowdury@users.noreply.github.com>
2021-04-21 00:40:11 -0400

    added simple word counter
    Former-commit-id: d92fb52e52e847ad14993f3d64a725f2fdb071e1

--- scripts/extract_feature.py
+++ scripts/extract_feature.py
@@ -1,2 +1,19 @@
-# this script will add feature to preprocessed file
+# this script will extract different features for each observations
+# potentially use mrjob/spark in future
+
+# feature 1 : simple word count
+import pandas as pd
+import numpy as np
+testdf = pd.read_csv('assets/test.csv')
+def text_counter(sentence):
+    count = len(sentence.split())
+    return count
+
+def word_count(df):
+    df['word_count']=df['original_text'].apply(text_counter)
+    return df
+
+import nltk
+
+# feature 2 : word count without stopwords and punctuations
 
