c809a2593030cca0c79b492d23d120463f302514
Asadur <asadurchowdury@gmail.com>
2021-05-07 13:54:43 -0400

    desktop mod

--- scripts/random_forest.py
+++ scripts/random_forest.py
@@ -12,25 +12,25 @@ vectorizer = TfidfVectorizer(min_df=100,ngram_range=(1,2),stop_words='english')
 print('Vectorizing the training data ...')
 X_train = vectorizer.fit_transform(data.original_text)
 y_train = data.label
-
-from sklearn.preprocessing import MaxAbsScaler, RobustScaler
-scaling = RobustScaler(with_centering=False).fit(X_train)
-X_train = scaling.transform(X_train)
+print(X_train.shape)
+# from sklearn.preprocessing import MaxAbsScaler, RobustScaler
+# scaling = RobustScaler(with_centering=False).fit(X_train)
+# X_train = scaling.transform(X_train)
 
 
 print('Training Random forest model ...')
 clf = RandomForestClassifier(n_estimators=10,n_jobs=-1)
 clf.fit(X_train,y_train)
 
-testdata = pd.read_csv('assets/WikiLarge_Train.csv')
+testdata = pd.read_csv('assets/WikiLarge_Test.csv')
 
 print('Fitting test data to vectorizer ...')
 X_test = vectorizer.transform(testdata.original_text)
-X_test = scaling.transform(X_test)
+# X_test = scaling.transform(X_test)
 y_test = testdata.label
 
 y_pred = clf.predict(X_test)
-
+prediction = pd.DataFrame(y_pred, columns=['label']).to_csv('prediction.csv')
 
 from sklearn.metrics import f1_score, accuracy_score, auc
 
