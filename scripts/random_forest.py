83dc5ecb4d6fd61d682ceccafd15ae9963c31a1e
asadurchowdury <69541380+asadurchowdury@users.noreply.github.com>
2021-05-23 14:29:33 -0400

    cleanup

--- scripts/random_forest.py
+++ scripts/random_forest.py
@@ -4,16 +4,12 @@ import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import GradientBoostingClassifier
-<<<<<<< HEAD
 from functools import cache, lru_cache
 
 # @ cache
 
 data = pd.read_csv('assets/test.csv')
 data = pd.read_csv('assets/train_with_features.csv')
-=======
-data = pd.read_csv('assets/test.csv')
->>>>>>> d5868e35d85adb1119c6b687750c6acca526f392
 
 # other features can be added to this vectorizer, checkout sklearn for np.hstack
 vectorizer = TfidfVectorizer(min_df=100,ngram_range=(1,2),stop_words='english')
@@ -39,14 +35,10 @@ X_test = vectorizer.transform(testdata.original_text)
 y_test = testdata.label
 
 y_pred = clf.predict(X_test)
-<<<<<<< HEAD
 prediction = pd.DataFrame(y_pred, columns=['label'])
 prediction.rename({"Unnamed: 0":'id'},axis=1,inplace=True)
 prediction.to_csv('prediction_random.csv')
 
-=======
-prediction = pd.DataFrame(y_pred, columns=['label']).to_csv('prediction.csv')
->>>>>>> d5868e35d85adb1119c6b687750c6acca526f392
 
 from sklearn.metrics import f1_score, accuracy_score, auc
 
@@ -57,9 +49,5 @@ print("F1 score for random forest model is ", score)
 
 
 
-<<<<<<< HEAD
 if __name__ == '__main__':
     main()
-=======
-
->>>>>>> d5868e35d85adb1119c6b687750c6acca526f392
